cf6c88feb0ec817076c0d1e192b627c0b4ac6709
Sennett Lau <laub1199@gmail.com>
2023-07-27 22:28:16 +0800

    added test for ArgIsValidMaxDepth

--- test/utils_args_check_test.go
+++ test/utils_args_check_test.go
@@ -34,3 +34,23 @@ func TestArgIsValidExtendIgnoreList(t *testing.T) {
 		}
 	}
 }
+
+func TestArgIsValidMaxDepth(t *testing.T) {
+	testCases := []argsCheckTestCase{
+		// Valid test cases
+		{input: "--max-depth=1", expected: true},
+		{input: "--max-depth=10", expected: true},
+
+		// Invalid test cases
+		{input: "key=3", expected: false},
+		{input: "--max-depth=0", expected: false},
+		{input: "--max-depth=11", expected: false},
+	}
+
+	for _, tc := range testCases {
+		result := utils.ArgIsValidMaxDepth(tc.input)
+		if result != tc.expected {
+			t.Errorf("Input: %s, Expected: %v, Got: %v", tc.input, tc.expected, result)
+		}
+	}
+}
